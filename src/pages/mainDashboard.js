cb9cc5b1f72536cbcd6b9ddf49fb2cd5fd3c59d2
lastriantini <lastriantini@smkwikrama.sch.id>
2024-09-04 19:13:15 +0700

    add: cardflip games

--- src/pages/mainDashboard.js
+++ src/pages/mainDashboard.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Home from '../components/home';
 import Timeline from '../components/Timeline';
 import Tokoh from '../components/tokoh';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 function MainDashboard() {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
-    const handleClick = () => {
-        navigate('/games');
-    };
+    // const handleClick = () => {
+    //     navigate('/games');
+    // };
 
     return (
         <>
@@ -18,7 +18,7 @@ function MainDashboard() {
                 <Timeline />
             </section>
             <button
-                onClick={handleClick}
+                // onClick={handleClick}
                 className='shadow-black shadow text-center bg-[#7D0A0A] text-white text-[1.5vw] mx-[40vw] w-[18vw] mb-[3vw] mt-[-2vw] rounded-full transition ease-in-out delay-150 hover:-translate-y-1 hover:bg-white hover:border-1 hover:text-[#7D0A0A]'
             >
                 <div className='m-[0.3vw] px-[4vw] py-[0.7vw] rasa-regular font-semibold rounded-full border border-white hover:border-red-900'>
