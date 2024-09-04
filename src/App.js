21b28e86652b3d518e335903a56c598bd73ff3d0
lastriantini <lastriantini@smkwikrama.sch.id>
2024-09-04 19:07:17 +0700

    add: cardflip games

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import MainDashboard from './pages/mainDashboard.js';
 import MiniGame from './components/MiniGames.js';
+
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainDashboard />} />
           <Route path="/games" element={<MiniGame />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </QueryClientProvider>
   );
 }
--- src/pages/mainDashboard.js
+++ src/pages/mainDashboard.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Home from '../components/home';
 import Timeline from '../components/Timeline';
 import Tokoh from '../components/tokoh';
@@ -7,7 +8,7 @@ function MainDashboard() {
     const navigate = useNavigate();
 
     const handleClick = () => {
-      navigate('/games');
+        navigate('/games');
     };
 
     return (
@@ -16,14 +17,19 @@ function MainDashboard() {
             <section id='timeline' style={{ margin: "6vw 0 0 0" }}>
                 <Timeline />
             </section>
-            <button onClick={handleClick} className='shadow-black shadow text-center bg-[#7D0A0A] text-white text-[1.5vw] mx-[40vw] w-[18vw] mb-[3vw] mt-[-2vw] rounded-full transition ease-in-out delay-150 hover:-translate-y-1 hover:bg-white hover:border-1 hover:text-[#7D0A0A]'>
-                <div className='m-[0.3vw] px-[4vw] py-[0.7vw] rasa-regular font-semibold rounded-full border border-white hover:border-red-900'>MINI GAME</div>
+            <button
+                onClick={handleClick}
+                className='shadow-black shadow text-center bg-[#7D0A0A] text-white text-[1.5vw] mx-[40vw] w-[18vw] mb-[3vw] mt-[-2vw] rounded-full transition ease-in-out delay-150 hover:-translate-y-1 hover:bg-white hover:border-1 hover:text-[#7D0A0A]'
+            >
+                <div className='m-[0.3vw] px-[4vw] py-[0.7vw] rasa-regular font-semibold rounded-full border border-white hover:border-red-900'>
+                    MINI GAME
+                </div>
             </button>
             <section id='tokoh' style={{ margin: "0vw 0 0 0" }}>
                 <Tokoh />
             </section>
         </>
-    )
+    );
 }
 
-export default MainDashboard
+export default MainDashboard;
